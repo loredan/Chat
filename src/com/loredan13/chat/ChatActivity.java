867fd382b9f4b333df84dd4fe0454ef214d28a5c
loredan13 <loredan725@gmail.com>
2015-03-12 15:46:21 +0300

    Initial commit

---
+++ src/com/loredan13/chat/ChatActivity.java
@@ -0,0 +1,12 @@
+package com.loredan13.chat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChatActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
